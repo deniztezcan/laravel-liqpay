b2d766f7060b4975d93da2fc4839fa37f44cdcb4
Deniz Tezcan <howdy@deniztezcan.me>
2020-09-09 14:52:22 +0200

    Finalzied the base package

---
+++ config/liqpay.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'public_key' 	=> env('LIQPAY_PUBLIC', ''),
+	'private_key' 	=> env('LIQPAY_PRIVATE', ''),
+];
--- src/LiqPay.php
+++ src/LiqPay.php
@@ -2,12 +2,31 @@
 
 namespace DenizTezcan\LiqPay;
 
+use DenizTezcan\LiqPay\Support\LiqPay as AbstractLiqPay;
+
 class LiqPay
 {
     protected $client = null;
 
     public function __construct()
     {
+    	$this->client = new AbstractLiqPay(config('liqpay.public_key'), config('liqpay.private_key'));
+    }
+
+    public function pay(
+    	$amount 		= 1.00,
+    	$currency 		= 'UAH',
+    	$description 	= 'foo',
+    	$order_id 		= "bar"
+    ) {
+    	$this->client->cnb_form(array(
+		'action'         => 'pay',
+		'amount'         => $amount,
+		'currency'       => $currency,
+		'description'    => $description,
+		'order_id'       => $order_id,
+		'version'        => '3'
+		));
     }
 
 }
