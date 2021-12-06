9c9df36cf2eadbb680f24e8e1cc01b1a399e0a69
kSolovei <kateryna@tezcanit.solutions>
2021-12-06 19:01:51 +0200

    Added validation data

--- src/LiqPayPost.php
+++ src/LiqPayPost.php
@@ -6,22 +6,18 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-
 require '../vendor/autoload.php';
-
-
-// dd('trololo');
 
 use GuzzleHttp\Client as Client;
 use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use DenizTezcan\LiqPay\Support\LiqPay as AbstractLiqPay;
 
+// include('../config/liqpay.php');
 class LiqPayPost
 {
-    public function __construct(
-        // protected ...$client = null,
-    )
+
+    public function __construct()
     {
         // $this->client = new AbstractLiqPay('sandbox_i35444360364', 'sandbox_oes9JoLcbl3oCgpafe8xfQW5sHpifiR34KDKWHhF');
     }
@@ -31,13 +27,13 @@ class LiqPayPost
         string $currency = 'UAH',
         string $description = 'foo',
         string $order_id = "bar",
-        string $result_url = "",
+        string $result_url = "https://www.liqpay.ua/ru/checkout/card/sandbox_i35444360364",
         string $server_url = "",
-    ): void {
-        
-
+        string $public_key = "",
+        string $private_key = "",
+    ): string {
         $client = new Client([
-            'base_uri' => 'http://localhost:8000/',//$_checkout_url = 'https://www.liqpay.ua/api/3/checkout',
+            'base_uri' => 'http://localhost:8000/', //$_checkout_url = 'https://www.liqpay.ua/api/3/checkout',
             'form_params' => [
                 'action' => 'pay',
                 'amount' => $amount,
@@ -48,10 +44,12 @@ class LiqPayPost
                 'result_url' => $result_url,
                 'server_url' => $server_url,
             ],
+            'language' => 'en',
+
         ]);
 
-        //зачем ты вместо урла указала путь к файлу ? чтобы загрузилась формма..  я не это зне нтаак ю, что указывать покажу в доку
-        $response = $client->request('POST', 'https://www.liqpay.ua/api/3/checkout', [
+        // send data with post method to the result url 
+        $request = $client->post('https://www.liqpay.ua/api/3/checkout', [
             'form_params' => [
                 'action' => 'pay',
                 'amount' => $amount,
@@ -62,22 +60,33 @@ class LiqPayPost
                 'result_url' => $result_url,
                 'server_url' => $server_url,
             ],
+            'language' => 'en',
         ]);
-        $data = json_encode($response->getBody());
-        dd($data);
 
-        $liqPayClient = new AbstractLiqPay('https://www.liqpay.ua/api/3/checkout', null, $data['form_params'],'sandbox_i35444360364', 'sandbox_oes9JoLcbl3oCgpafe8xfQW5sHpifiR34KDKWHhF');
+        $data = $this->formData(
+            [
+                'form_params' => [
+                    'action' => 'pay',
+                    'amount' => $amount,
+                    'currency' => $currency,
+                    'description' => $description,
+                    'order_id' => $order_id,
+                    'version' => '3',
+                    'result_url' => $result_url,
+                    'server_url' => $server_url,
+                ],
+            ],
+        ); // answer from url
+
+
+        // $liqPayClient = new AbstractLiqPay('https://www.liqpay.ua/api/3/checkout', null, $data, $public_key, $private_key);
         $form = file_get_contents('form/index.html');
 
-        $data = $response->form_params;
-        dd($data);
-        // $data = '123';
+        // dd($liqPayClient);
         $script = '<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script><script type="text/javascript">jQuery(document).ready(function($) {$("form").submit();});</script> ';
-        $html = $form.$script;
-        // return $html;
-        // var_dump($html);
-        // return $data;
-        // return $data;
+        
+        $form = file_get_contents('form/index.html');$html = $form . $script;
+        return $html;
     }
 
     public function status(string $order_id = "bar"): string|false
@@ -90,6 +99,76 @@ class LiqPayPost
 
         return json_encode($data, TRUE);
     }
+
+    public function formData(array $params): string
+    {
+        $language = 'ru';
+        if (isset($params['language']) && $params['language'] === 'en') {
+            $language = 'en';
+        }
+
+        $params    = $this->validateParams($params); //if all params exists
+        $data      = $this->getData($params); // make string from array && UTF-8
+        // $signature = $this->createSignature($params); I think we don't need it
+        return $data;
+    }
+
+    public function validateParams(array $params): array
+    {
+        $suppotedCurrencyArray = [
+            'EUR',
+            'USD',
+            'UAH',
+            'RUB',
+            'RUR',
+        ];
+
+        // ($params['version'] !== null) ?? throw new InvalidArgumentException('version is null');
+        // ($params['amount'] !== null) ?? throw new InvalidArgumentException('payment amount not specified');
+        // (($params['currency'] !== null) && in_array($params['currency'], $suppotedCurrencyArray)) ?? throw new InvalidArgumentException('unsupported currency');
+        // ($params['currency'] === 'RUR') ?? $params['currency'] = 'RUB';
+        // ($params['description'] !== null) ?? throw new InvalidArgumentException('description is empty');
+        // ($params['result_url'] !== null) ?? throw new InvalidArgumentException('result_url not specified');
+        // ($params['server_url'] !== null) ?? throw new InvalidArgumentException('server_url not specified');
+        // ($params['order_id'] !== null) ?? throw new InvalidArgumentException('order_id not specified');
+
+        ($params['version'] !== null) ?? die('version is null');
+        ($params['amount'] !== null) ?? die('payment amount not specified');
+        (($params['currency'] !== null) && in_array($params['currency'], $suppotedCurrencyArray)) ?? die('unsupported currency');
+        ($params['currency'] === 'RUR') ?? $params['currency'] = 'RUB';
+        ($params['description'] !== null) ?? die('description is empty');
+        ($params['result_url'] !== null) ?? die('result_url not specified');
+        ($params['server_url'] !== null) ?? die('server_url not specified');
+        ($params['order_id'] !== null) ?? die('order_id not specified');
+
+        return $params;
+    }
+
+    function checkInput($input)
+    {
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+        return $input;
+    }
+
+    public function getData(array $params): string
+    {
+        return base64_encode(json_encode($params));
+    }
+
+    // public function createSignature(array $params): array
+    // {
+    //     $params      = $this->validateParams($params);
+    //     $private_key = $this->_private_key;
+
+    //     $json      = $this->encode_params($params);
+    //     $signature = $this->str_to_sign($private_key . $json . $private_key);
+
+    //     base64_encode(sha1($str, 1))
+
+    //     return $signature;
+    // }
 }
 $pay = new LiqPayPost();
 $pay->pay();
